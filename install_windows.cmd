e21cfbbafc38670ea53d8e124bc3e87ebb321b9e
Richard Erkhov <104026872+RichardErkhov@users.noreply.github.com>
2023-06-21 12:10:59 +0300

    Update install_windows.cmd

--- install_windows.cmd
+++ install_windows.cmd
@@ -3,6 +3,7 @@ python -m virtualenv venv
 call venv\scripts\activate.bat
 curl --location --remote-header-name --remote-name https://github.com/RichardErkhov/FastFaceSwap/releases/download/model/inswapper_128.onnx
 curl --location --remote-header-name --remote-name https://github.com/RichardErkhov/FastFaceSwap/releases/download/model/GFPGANv1.4.pth
+curl --location --remote-header-name --remote-name https://github.com/RichardErkhov/FastFaceSwap/releases/download/model/256_v2_big_generator_pretrain_stage1_38499.h5
 pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu117
 pip install -r requirements.txt
 pip install tensorflow-gpu==2.10.1
