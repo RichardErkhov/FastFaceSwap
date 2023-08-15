75541689e6f6b18d4639cd44735a8fcd29e7a162
Richard Erkhov <104026872+RichardErkhov@users.noreply.github.com>
2023-08-15 16:33:53 +0300

    Add files via upload

--- globalsz.py
+++ globalsz.py
@@ -1,6 +1,6 @@
 import threading
 # config
-select_face_swapper_gpu = None #None for all gpus. Or make it a list, like [0, 1, 2] to select gpus to use
+select_face_swapper_gpu = [0] #None for all gpus. Or make it a list, like [0, 1, 2] to select gpus to use
 select_gfpgan_gpu = 0 #supports only 1 gpu for now
 select_realesrgan_gpu = 0 # supports only 1 gpu for now
 realeasrgan_model = "RealESRGAN_x2plus"
@@ -17,3 +17,4 @@ realeasrgan_enhancer = None
 THREAD_SEMAPHORE = threading.Semaphore()
 realesrgan_lock = threading.Semaphore()
 cuda = True # just debugging I think 
+source_face = None
