424f6c05b39b2575bc0c8b5f4d09a4976b37a267
Richard Erkhov <104026872+RichardErkhov@users.noreply.github.com>
2023-08-20 10:50:50 +0300

    Add files via upload

--- install_linux.sh
+++ install_linux.sh
@@ -1,15 +1,13 @@
-#!/bin/bash
 python -m pip install virtualenv
 python -m virtualenv venv
-call venv/bin/activate
-curl -LO https://github.com/RichardErkhov/FastFaceSwap/releases/download/model/inswapper_128.onnx
-curl -LO https://github.com/RichardErkhov/FastFaceSwap/releases/download/model/GFPGANv1.4.pth
-curl -LO https://github.com/RichardErkhov/FastFaceSwap/releases/download/model/complex_256_v7_stage3_12999.h5
-curl -LO https://github.com/RichardErkhov/FastFaceSwap/releases/download/model/GFPGANv1.4.onnx
+source venv/bin/activate
+wget https://github.com/RichardErkhov/FastFaceSwap/releases/download/model/inswapper_128.onnx
+wget https://github.com/RichardErkhov/FastFaceSwap/releases/download/model/GFPGANv1.4.pth
+wget https://github.com/RichardErkhov/FastFaceSwap/releases/download/model/complex_256_v7_stage3_12999.h5
+wget https://github.com/RichardErkhov/FastFaceSwap/releases/download/model/GFPGANv1.4.onnx
 pip install torch torchvision torchaudio
-pip install onnxruntime-silicon
 pip install -r requirements.txt
-pip install tensorflow-gpu==2.12
+pip install tensorflow-gpu==2.10.1
 pip install protobuf==3.20.2
 pip uninstall opencv-python opencv-headless-python opencv-contrib-python -q -y
 pip install opencv-python
--- install_mac.sh
+++ install_mac.sh
@@ -1,2 +1,15 @@
+#!/bin/bash
 python -m pip install virtualenv
 python -m virtualenv venv
+source venv/bin/activate
+curl -LO https://github.com/RichardErkhov/FastFaceSwap/releases/download/model/inswapper_128.onnx
+curl -LO https://github.com/RichardErkhov/FastFaceSwap/releases/download/model/GFPGANv1.4.pth
+curl -LO https://github.com/RichardErkhov/FastFaceSwap/releases/download/model/complex_256_v7_stage3_12999.h5
+curl -LO https://github.com/RichardErkhov/FastFaceSwap/releases/download/model/GFPGANv1.4.onnx
+pip install torch torchvision torchaudio
+pip install onnxruntime-silicon
+pip install -r requirements.txt
+pip install tensorflow-gpu==2.12
+pip install protobuf==3.20.2
+pip uninstall opencv-python opencv-headless-python opencv-contrib-python -q -y
+pip install opencv-python
