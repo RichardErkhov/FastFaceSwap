7658468cc5aaca1e5752bc0e8cebf5cc9f9be534
Richard Erkhov <104026872+RichardErkhov@users.noreply.github.com>
2023-08-20 11:08:36 +0300

    Add files via upload

---
+++ install_termux.sh
@@ -0,0 +1,14 @@
+python -m pip install virtualenv
+python -m virtualenv venv
+source venv/bin/activate
+wget https://github.com/RichardErkhov/FastFaceSwap/releases/download/model/inswapper_128.onnx
+wget https://github.com/RichardErkhov/FastFaceSwap/releases/download/model/GFPGANv1.4.pth
+wget https://github.com/RichardErkhov/FastFaceSwap/releases/download/model/complex_256_v7_stage3_12999.h5
+wget https://github.com/RichardErkhov/FastFaceSwap/releases/download/model/GFPGANv1.4.onnx
+pip install torch torchvision torchaudio
+pip install onnxruntime
+pip install -r requirements.txt
+pip install tensorflow==2.10.1
+pip install protobuf==3.20.2
+pip uninstall opencv-python opencv-headless-python opencv-contrib-python -q -y
+pip install opencv-python
